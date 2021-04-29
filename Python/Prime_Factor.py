3326b58b0be5865794fa50c28b5d07508eba72ae
tanya1019 <tanya.vanneri19@gmail.com>
2021-04-29 10:21:26 +0530

    [ADD] Solution for Prime Factor

---
+++ Python/Prime_Factor.py
@@ -0,0 +1,14 @@
+  # Prime Factor
+def primemum(a):
+    number = []
+    for x in range(2, int(a)):
+        i = 0
+        if a % x == 0:
+            for a in range(1, int(a)):
+                if x % a == 0:
+                    i = i + 1
+            if i == 1:
+                number.append(a)
+    print(max(number))
+
+primemum(600851475143)
