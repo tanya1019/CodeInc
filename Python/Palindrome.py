464da0fa7606f19c952b52790fbab6b2fddbc282
tanya1019 <tanya.vanneri19@gmail.com>
2021-04-29 10:19:29 +0530

    [ADD] Solution for Palindrome

---
+++ Python/Palindrome.py
@@ -0,0 +1,22 @@
+list1 = []
+list2 = []
+def mult():
+    global list1
+    for i in range(1000, 100 , -1):
+        for j in range(1000, 100 ,-1):
+            q = i*j
+            list1.append(q)
+    return(list1)
+
+print(mult())
+
+def Pal():
+    global list2
+    for w in list1:
+        w = str(w)
+        h = w[::-1]
+        if w == h:
+          list2.append(int(h))
+    return list2
+
+print(max(Pal()))
