26800ccdcdd2668ced0b1a6f149eb6301ca41bcb
tanya1019 <tanya.vanneri19@gmail.com>
2021-04-30 13:32:24 +0530

    FizzBuzz

---
+++ Python/FizzBuzz.py
@@ -0,0 +1,12 @@
+for x in range(1, 50):
+    if x%3 ==0 and x%5 == 0 :
+        print('FizzBuzz')
+        
+    elif x%5 == 0:
+        print('Buzz')
+        
+    elif x%3 == 0:
+        print('Fizz')
+        
+    else:
+        print(x)
